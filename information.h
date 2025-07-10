aedb3f2156665a4a6c7da9f304f9c7698dc99519
Mtao234Charct <admin@mtaoddn.site>
2025-07-10 12:43:33 +0800

    Add files via upload

---
+++ information.h
@@ -0,0 +1,8 @@
+#include "httplib.h"
+#include <windows.h>
+#include <map>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <fstream>
---
+++ openssl.h
@@ -0,0 +1,3 @@
+#include "C:/Program Files/OpenSSL-Win64/include/openssl/ssl.h"
+#pragma comment(lib, "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MD/libssl.lib")
+#pragma comment(lib, "C:/Program Files/OpenSSL-Win64/lib/VC/x64/MD/libcrypto.lib")
